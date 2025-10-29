86573ade60573959e3f8b5ff345590deb425101c
Raghul.A.R <arraghul6@gmail.com>
2025-10-29 21:30:26 +0530

    added new problem in basic recursion

--- Basics Recursion/recursion.py
+++ Basics Recursion/recursion.py
@@ -1,24 +1,78 @@
-# print a name n times using recursion
-def print_name(name ,count):
-    if count ==0:
-        return
-    print(name)
-    print_name(name , count- 1)
+# # print a name n times using recursion
+# def print_name(name ,count):
+#     if count ==0:
+#         return
+#     print(name)
+#     print_name(name , count- 1)
+# print_name("Alice", 5)
 
-# print numbers from 1 to n using recursion
-def print_num(i , n):
-    if i> n:
-        return
-    print(i)
-    print_num(i + 1 , n)
+# print("Next problem")
 
-print_name("Alice", 5)
-print_num(1 , 5)
+# # print numbers from 1 to n using recursion
+# def print_num(i , n):
+#     if i> n:
+#         return
+#     print(i)
+#     print_num(i + 1 , n)
 
-# print in reverse from n to 1 using recursion
-def print_reverse(i , n):
-    if i < n:
-        return
-    print(i)
-    print_reverse(i - 1 , n)
-print_reverse(5 ,1)
+# print_num(1 , 5)
+
+# print("Next problem")
+
+# # print in reverse from n to 1 using recursion
+# def print_reverse(i , n):
+#     if i < n:
+#         return
+#     print(i)
+#     print_reverse(i - 1 , n)
+# print_reverse(5 ,1)
+
+# print("Next problem")
+
+# # print from 1 to n using backtracking
+# def print_backtrack(i , n):
+#     if i < 1:
+#         return
+#     print_backtrack(i-1 , n)
+#     print(i)
+
+# print_backtrack(5 , 5)
+
+# print("Next problem")
+
+# # print from n to 1 using backtracking
+# def print_backtrack_reverse(i , n):
+#     if(i > n):
+#         return
+#     print_backtrack_reverse(i + 1 , n)
+#     print(i)
+
+# print_backtrack_reverse(1, 5)
+
+# print("NExt problem")
+
+# # print sum of first n numbers
+# #1
+# def print_sum(i , n, total=0):
+#     if(i > n):
+#         return total
+#     return print_sum(i +1 , n , total + i)
+    
+# ans = print_sum(1 , 5)
+# print(ans)
+
+# #2 parametrized sum
+# def print_sum(i , sum ):
+#     if(i < 1):
+#         return sum
+#     return print_sum(i - 1 , sum + i )
+# ans = print_sum(6 , 0)
+# print(ans)
+
+# functional sum
+
+# def func_sum(n):
+#     if n == 0:
+#         return 0 
+#     return (n + func_sum(n-1))
+# print(func_sum(5))
---
+++ Basics Recursion/recursion_2.py
@@ -0,0 +1,23 @@
+# # factorial of n
+
+# def fact_num(n):
+#     if n==1:
+#         return 1
+#     return n * fact_num(n-1)
+# n = int(input())
+# print(fact_num(n))
+
+#normally reverse an array 
+
+def rev_arr(arr):
+    start = 0 ;
+    end = len(arr) - 1
+
+    while (start < end):
+        arr[start] , arr[end] = arr[end] , arr[start]
+        start += 1
+        end -=1
+
+my_list = [1, 2, 3, 4 ,5 ]
+rev_arr(my_list)
+print(my_list)
