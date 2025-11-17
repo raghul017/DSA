b3ede8be54e0967460382b4f4948affe272bf8a6
Raghul.A.R <arraghul6@gmail.com>
2025-11-17 23:25:25 +0530

    added array problem for finding largest and second largest element

--- Array/largest_ele.py
+++ Array/largest_ele.py
@@ -1,3 +1,5 @@
+#for largest and also second largest 
+
 arr = [1 ,2, 3, 4, 5, 6]
 
 large_element = arr[0]   
@@ -6,4 +8,26 @@ for i in range(len(arr)):
     if arr[i] > large_element:
         large_element = arr[i]
 
-print(large_element)
+second_largest = 0
+
+for i in range(len(arr)):
+    if (arr[i] > second_largest and arr[i] != large_element):
+        second_largest = arr[i]
+
+print(second_largest)
+
+
+#Second largest optimized solution
+arr = [1,2,3,4,5,6]
+
+largest = arr[0]
+second_largest = float('-inf')
+
+for i in arr:
+    if i > largest:
+        second_largest = largest
+        largest = i
+    elif i > second_largest and i != largest:
+        second_largest = i
+
+print(second_largest)
