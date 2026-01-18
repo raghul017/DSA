37a311d19390bbb3baca56aa7679de7e76e63d43
Raghul.A.R <arraghul6@gmail.com>
2026-01-18 17:13:11 +0530

    added linked list

--- Linked_List/basics.py
+++ Linked_List/basics.py
@@ -1,12 +1,45 @@
 class Node:
-    def __init__(self,data = None , next = None):
+    def __init__(self, data):
         self.data = data
-        self.next = next
-    
+        self.next = None
+
 class LinkedList:
+
     def __init__(self):
         self.head = None
     
+    def insert_at_beginning(self , new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+    
+    def insert_at_end(self , new_data):
+        new_node = Node(new_data)
+        if self.head is None:
+            self.head = new_node
+            return
 
-if __name__ == '__main__':
-    pass
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        
+        curr.next = new_node
+    
+    def print_list(self):
+        curr = self.head
+        while curr:
+            print(f"{curr.data} -> " , end=" ")
+            curr = curr.next
+        print("None")
+    
+if __name__ == "__main__":
+    llist = LinkedList()
+
+    llist.insert_at_beginning(10)
+    llist.insert_at_beginning(5) 
+    llist.insert_at_end(20)
+    llist.insert_at_end(30)
+
+    # Expected output: 5 -> 10 -> 20 -> 30 -> None
+    print("My Linked List:")
+    llist.print_list()
